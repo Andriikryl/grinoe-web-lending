acd5f32eb150fca94ff79870e680b0182f480bfb
Andriikryl <andreyk557@gmail.com>
2023-07-04 16:07:27 +0300

    add icons components

---
+++ src/app/icons/Arrow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Arrow() {
+  return (
+    <svg
+      width="57"
+      height="57"
+      viewBox="0 0 57 57"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M53 4L5 52M5 52H48.9626M5 52V8.03738"
+        stroke="#BFEA81"
+        stroke-width="10"
+      />
+    </svg>
+  );
+}
